2f1fa128b8098df1f0b1c770b8b544294c295bc3
pinkcuteloli <163749636+pinkcuteloli@users.noreply.github.com>
2024-04-01 13:44:53 +0900

    Add input-otp component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@/components/ui/input-otp";
 import { useEffect, useState } from "react";
 import { Store } from "tauri-plugin-store-api";
 import Onboarding from "./routes/onboarding";
@@ -15,7 +16,32 @@ function App() {
   });
 
   if (path) {
-    return <div>Path: {path}</div>;
+    return (
+      <div>
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <h2 className="text-xl font-semibold mb-4 text-center">ロックされています</h2>
+            <form>
+              <InputOTP maxLength={6}>
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+            </form>
+          </div>
+        </div>
+
+        <div>Path: {path}</div>
+      </div>
+    );
   }
   return <Onboarding />;
 }
